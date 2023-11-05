ec897136f42a6b4f15c62426b200d5f4ce04c963
Maksim <m.boltonogov@gmail.com>
2023-11-05 18:35:21 +0300

    Дз 49-50 создал динамический переход по страницам,сделал горизонтальное меню и задний фон, подключил статические файлы

--- texnologyshop/ComputerComponents/urls.py
+++ texnologyshop/ComputerComponents/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 import ComputerComponents.views as components
 
-
 urlpatterns = [
-    path('videocard/', components.videocard)
+    path('videocard/', components.videocard),
+    re_path(r"^videocardyear/(?P<year>[0-9]{4})/$", components.videocardyear),
+    path('test/', components.test),
+    path('<slug:name_page>', components.components_pages)
 ]
--- texnologyshop/ComputerComponents/views.py
+++ texnologyshop/ComputerComponents/views.py
@@ -1,13 +1,35 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+
 
 # Create your views here.
 def index(request):
     return HttpResponse('Страница про компоненты')
 
+
 def videocard(request):
     data = {
         'title': 'Videocard',
-        'menu': ['About','Page1','Page3']
+        'menu': ['About', 'Page1', 'Page3']
     }
-    return render(request,'computercomponents/index.html',data)
+    return render(request, 'computercomponents/index.html', data)
+
+
+def videocardyear(request, year):
+    if int(year) > 0:
+        return HttpResponse(f'видеокарта {year} года выпуска ')
+    else:
+        raise Http404('<h1>Ошибка 404</h1>')
+
+
+def components_pages(request, name_page):
+    data = {
+        'menu': ['Главная', 'Каталог', 'Товары']
+    }
+    if name_page in ['info_component', 'seach_component']:
+        return render(request, f'computercomponents/{name_page}.html', data)
+    return Http404('<h1>Page not found</h1>')
+
+
+def test(request):
+    return render(request, 'testcase/test.html')
