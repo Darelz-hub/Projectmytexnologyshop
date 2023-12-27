e8e03790754535cb0a6d1bcb15681cf3a658841a
Maksim <m.boltonogov@gmail.com>
2023-12-27 19:49:09 +0300

    Доработал стили на двух страницах documentation и documentationmain, настроил кнопки для передачи нужных данных на сервер, смог подключить разные стили для каждой из страниц

--- texnologyshop/ComputerComponents/urls.py
+++ texnologyshop/ComputerComponents/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, re_path
 import ComputerComponents.views as components
 
 urlpatterns = [
-    path('videocard/', components.videocard),
-    re_path(r"^videocardyear/(?P<year>[0-9]{4})/$", components.videocardyear),
+    # path('videocard/', components.videocard),
+    # re_path(r"^videocardyear/(?P<year>[0-9]{4})/$", components.videocardyear),
     path('test/', components.test),
-    path('<slug:name_page>', components.components_pages)
+    # path('<slug:name_page>', components.components_pages),
+    path('document/', components.documentationmain),
+    path('document/documentation/', components.documentation),
 ]
--- texnologyshop/ComputerComponents/views.py
+++ texnologyshop/ComputerComponents/views.py
@@ -4,44 +4,54 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse('Страница про компоненты')
+# def index(request):
+#     return HttpResponse('Страница про компоненты')
+#
+#
+# def videocard(request):
+#     data = {
+#         'title': 'Videocard',
+#         'menu': ['About', 'Page1', 'Page3']
+#     }
+#     return render(request, 'computercomponents/index.html', data)
+#
 
-
-def videocard(request):
-    data = {
-        'title': 'Videocard',
-        'menu': ['About', 'Page1', 'Page3']
-    }
-    return render(request, 'computercomponents/index.html', data)
-
-
-def videocardyear(request, year):
-    if int(year) > 0:
-        return HttpResponse(f'видеокарта {year} года выпуска ')
-    else:
-        raise Http404('<h1>Ошибка 404</h1>')
-
+# def videocardyear(request, year):
+#     if int(year) > 0:
+#         return HttpResponse(f'видеокарта {year} года выпуска ')
+#     else:
+#         raise Http404('<h1>Ошибка 404</h1>')
+#
 
 def get_documentation(request):
-    # name = requests.GET.get('get_info', 'Central_processing_unit')
-    result = {}
-    req = requests.post(
-        f'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=10&exlimit=2&titles=Central_processing_unit&explaintext=1&format=json')
-    req = req.json()
-    indexkey = list(req['query']['pages'])[0]  # Берем ключ, который состоит из цифр в словаре pages
-    # print(req['query']['pages'][indexkey])
-    result.update({'title': req['query']['pages'][indexkey]['title'],
-                   'content': req['query']['pages'][indexkey]['extract']})
-    print(result)
-    return {'response': result}
+    if request.method == 'POST':
+        name = request.POST.get('name', 'Central_processing_unit')
+        result = {}
+        req = requests.post(
+            f'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=10&exlimit=2&titles={name}&explaintext=1&format=json')
+        req = req.json()
+        indexkey = list(req['query']['pages'])[0]  # Берем ключ, который состоит из цифр в словаре pages
+        result.update({'title': req['query']['pages'][indexkey]['title'],
+                       'content': req['query']['pages'][indexkey]['extract']})
+        return {'response': result}
+    return {'response': ''}
 
 
-def components_pages(request, name_page):
+def documentationmain(request):
+    # data = get_documentation(request)
+    return render(request, 'computercomponents/documentationmain.html')
+
+
+def documentation(request):
     data = get_documentation(request)
-    if name_page in ['info_component', 'seach_component', 'documentation']:
-        return render(request, f'computercomponents/{name_page}.html', data)
-    return Http404('<h1>Page not found</h1>')
+    return render(request, 'computercomponents/documentation.html', data)
+
+
+# def components_pages(request, name_page):
+#     data = get_documentation(request)
+#     if name_page in ['info_component', 'seach_component', 'documentation']:
+#         return render(request, f'computercomponents/{name_page}.html', data)
+#     return Http404('<h1>Page not found</h1>')
 
 
 # def components_pages(request, name_page):
